3ac1422f2c9c2bf0e1cf81679e5d6936ca69558d
gabriela <gkcc1@gcloud.ua.es>
2025-07-26 20:17:07 +0200

    add vercel frontend domain to CORS_ALLOWED_ORIGINS

--- back_django/settings.py
+++ back_django/settings.py
@@ -143,14 +143,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5174",
-    "https://app-price-frontend.vercel.app",  # o tu dominio real
+    "https://app-price-psi.vercel.app",  # o tu dominio real
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5174",  # Agrega esta línea
-    "https://app-price-frontend.vercel.app",  # o tu dominio real
+    "https://app-price-psi.vercel.app",  # o tu dominio real
 ]
 
 REST_FRAMEWORK = {
